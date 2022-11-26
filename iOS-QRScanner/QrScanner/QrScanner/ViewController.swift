e1e81b6d688aa5b7506ff49f37830c9459d63154
Alexey Borisov <alexey.borisov@umbrellait.com>
2022-11-26 13:45:38 +0300

    -
    Scanner is work

--- iOS-QRScanner/QrScanner/QrScanner/ViewController.swift
+++ iOS-QRScanner/QrScanner/QrScanner/ViewController.swift
@@ -5,15 +5,111 @@
 //  Created by Alexey Borisov on 26.11.2022.
 //
 
+import AVFoundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    @IBOutlet weak var qrString: UILabel!
+    @IBOutlet weak var buttonLabel: UIButton!
+    @IBOutlet weak var previewForCaptureQR: UIView!
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        buttonLabel.isHidden = true
+        captureSession.stopRunning()
+        captureSession.startRunning()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        captureSession = AVCaptureSession()
+
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed()
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+        } else {
+            failed()
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = previewForCaptureQR.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        previewForCaptureQR.layer.addSublayer(previewLayer)
+
+        captureSession.startRunning()
     }
 
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning()
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+
+        dismiss(animated: true)
+    }
+
+    func found(code: String) {
+        qrString.text = code
+        buttonLabel.isHidden = false
+        buttonLabel.titleLabel?.text = "Сканировать заново"
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
 }
 
